d30c8488bfd3c886d1382c0bc232c51f0e8d1528
fire567 <dante.nero.95@gmail.com>
2021-01-27 22:49:36 +0300

    start hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Accordion from './components/Accordion';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const items = [
+    {
+        title: 'What is React?',
+        content: 'React is a front end javascript framework'
+    },
 
-export default App;
+    {
+        title: 'Why use React?',
+        content: 'React is a favourite JS library among engineers'
+    },
+
+    {
+        title: 'How do you use React?',
+        content: 'You are uses Ract by creating components'
+    }
+]
+
+export default () => {
+    return (
+    <h1>
+        <Accordion items={items} />
+    </h1>
+    )
+};
