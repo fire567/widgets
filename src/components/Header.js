590b1d5566e279948ea14659786963d3c0773140
fire567 <dante.nero.95@gmail.com>
2021-02-10 09:40:19 +0300

    navigation

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const Header = () => {
+    return (
+        <div className="ui secondary pointing menu">
+            <a className="item" href="/">Accordion</a>
+            <a className="item" href="/list">Search</a>
+            <a className="item" href="/dropdown">Dropdown</a>
+            <a className="item" href="/translate">Translate</a>
+        </div>
+    )
+}
+
+export default Header;
